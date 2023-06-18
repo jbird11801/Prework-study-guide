be09a204dfd43c35a01d31dda9136282dc6751e1
jbird11801@gmail.com <jbird11801@gmail.com>
2023-06-18 02:39:33 -0400

    finalized JS

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
-var topic = ["HTML","CSS","Git","JavaScript"];
+var topics = ["HTML","CSS","Git","JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-
-for(var counter = 0; counter<topic.length;counter++){
-console.log(topic[counter]);
+function listTopics() {
+  for(var counter = 0; counter<topics.length;counter++){
+    console.log(topics[counter]);
+  }
 }
-/*
-if (topic === 'HTML') {
+
+function selectTopic(){
+if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
-}*/
+}
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
